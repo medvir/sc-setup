d1d9b4befd6097e549fcd182586e4b260a767a91
ozagordi <osvaldo.zagordi+git@gmail.com>
2016-10-19 17:24:31 +0200

    More users, and velvet

--- timavosetup.sh
+++ timavosetup.sh
@@ -31,6 +31,8 @@ then
     smalt \
     tabix \
     unzip \
+    velvet \
+    velvetoptimiser \
     zlib1g-dev
 
     apt autoremove
@@ -100,6 +102,10 @@ addgroup --gid 1001 ngs
 
 yes | adduser --uid 501 --gid 1001 --disabled-password ozagordi --quiet
 yes | adduser --uid 502 --gid 1001 --disabled-password mihuber --quiet
+yes | adduser --uid 503 --gid 1001 --disabled-password geifa --quiet
+yes | adduser --uid 504 --gid 1001 --disabled-password merles --quiet
+yes | adduser --uid 505 --gid 1001 --disabled-password vkufne --quiet
+yes | adduser --uid 506 --gid 1001 --disabled-password dlewan --quiet
 
 usermod -G sudo ozagordi
 usermod -G sudo mihuber
@@ -126,3 +132,46 @@ chmod 400 ~mihuber/.ssh/authorized_keys
 bash -c "echo -e 'export PATH=\$PATH:/usr/local/edirect:/opt/miniconda/bin' >> ~mihuber/.bash_profile"
 chown -R mihuber:ngs ~mihuber/.bash_profile
 
+echo "Adding key for geifa"
+mkdir -p ~geifa/.ssh/
+cat > ~geifa/.ssh/authorized_keys <<EOF
+ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCvZCdjgTRJ4ZvXfEUUaXjFuiBquv67XwMz9QAWG54qiw7G7B3PTQCdaq9O00OPMqiXCzS3XXmUhR5xQ0gkPhwjPQNb+av2a4/Bll78FUR8z/0ddW2lXZQ/BE3gej+JYOsERhoSr5yV76grscv5aOhH8u0Pw5I6GEl48KeLw2fDS2AT3D2xIHiibRctJCzCXnN72X6YTE5NzJXkbb79kbile5SfFks7z50hBgRMkmPODKpwxMXlu8aoJ5h1OH2S803mqpIueaOnvw7GdYPEBAaktjRRxswb6u3uCWUVqQxSvd078zE/et5sCOpwjqa6roWrXI4gZf/1giVp1598KGVr geissberger.fabienne@virologymc28.local
+EOF
+chown -R geifa:ngs ~geifa/.ssh
+chmod 700 ~geifa/.ssh/
+chmod 400 ~geifa/.ssh/authorized_keys
+bash -c "echo -e 'export PATH=\$PATH:/usr/local/edirect:/opt/miniconda/bin' >> ~geifa/.bash_profile"
+chown -R geifa:ngs ~geifa/.bash_profile
+
+echo "Adding key for merles"
+mkdir -p ~merles/.ssh/
+cat > ~merles/.ssh/authorized_keys <<EOF
+ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC9CE3ZBGLUWP5HOOIR6Ze4TzNAuII+PwAjuq5xcjeNvM06Pnp/3pqn/nkZAHUD9aX5rmM867i4ofvHPV596UVnbUjQ1L3YNtwLsvomuQRrYknUnI9dl0UpRjoPQ8Sml+DWuCeXXTTfO+ZPCAHFXZMp/MEVx91UmfqaGtesADJ6/i4bsTc3jLbePeNcIYe0DVXqDVkQYBGWGC+/3sfx2Ue4AZpUprA1dXpYiTApRohzeMco3GG5VsfWn6aaAQmWHT+SGeIko87xq5lEPoc+f6fOqfeGvYldjP8jdjuPIrE587gbX1da93swnsUzR4p5Ni/Jaq+qzD4VdbReFHhoEfeD merles@virologymc01.local
+EOF
+chown -R merles:ngs ~merles/.ssh
+chmod 700 ~merles/.ssh/
+chmod 400 ~merles/.ssh/authorized_keys
+bash -c "echo -e 'export PATH=\$PATH:/usr/local/edirect:/opt/miniconda/bin' >> ~merles/.bash_profile"
+chown -R merles:ngs ~merles/.bash_profile
+
+echo "Adding key for vkufne"
+mkdir -p ~vkufne/.ssh/
+cat > ~vkufne/.ssh/authorized_keys <<EOF
+ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDO/wMwbyUGs5cB33NMH/IT9Diyv0wn3me5GlsXUvdLEh/YZ1qWwpsA4sgkLH2bjw9Xo89/uz0KHDS3zEXNKXWA+mF8wQ3b/vqyiwXVg/lv3fzdTqgmDgs5Obs/SieYzq2+RiICKnn7nJDcN59z44w9/IAMrD+wCeCj4ZLpyFYjkjMhoQTCu8IB68/OJ8Iugl/JWKh7ApcmYREU/jHru0GEQa9Fz1pAAtfomX2kFAh+5CuztykwYxLCzMjTVROzhg2W0vfu/ycuUw9OFwA015Hi0aUritGPt1EeXDmd1Unrqd8c+omrL2OR+/aXJOdliBCSMMJdEjvMdkSgWBmCofRb verenakufner@virologymc17.local
+EOF
+chown -R vkufne:ngs ~vkufne/.ssh
+chmod 700 ~vkufne/.ssh/
+chmod 400 ~vkufne/.ssh/authorized_keys
+bash -c "echo -e 'export PATH=\$PATH:/usr/local/edirect:/opt/miniconda/bin' >> ~vkufne/.bash_profile"
+chown -R vkufne:ngs ~vkufne/.bash_profile
+
+echo "Adding key for dlewan"
+mkdir -p ~dlewan/.ssh/
+cat > ~dlewan/.ssh/authorized_keys <<EOF
+ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC1Bj0DKfrR98QtHBt6/6wlYi8cnPNIzGhyydyk4V30nk9q/tWe7FtkXVMklcthUEHYRbYaA6wcMmFtrjOEV9jGDinq6O3C7H70WfadiaKKUgbBYNUB9tV4eCMZKyhohNg+qtreYp/8N4We0NM1PbgZVsAWPWe8oCVxUTr3zblXiXil++bGKRalu0CLbWGTQNcHuNaKbcdngjxArePiar/dbXvFo78DJR21dnYy0x/nOY9K80qZoyj737rMZ4yr7BR7bcg20hF1xD60JlhCf+c/cBF8m03pMnmil/JO6gqacuB1QqOiPOetgq0G9/yb1DObEN9JYqkMq7jVl0/+Qy1v lewandowska.dagmara@virologymc21.local
+EOF
+chown -R dlewan:ngs ~dlewan/.ssh
+chmod 700 ~dlewan/.ssh/
+chmod 400 ~dlewan/.ssh/authorized_keys
+bash -c "echo -e 'export PATH=\$PATH:/usr/local/edirect:/opt/miniconda/bin' >> ~dlewan/.bash_profile"
+chown -R dlewan:ngs ~dlewan/.bash_profile
